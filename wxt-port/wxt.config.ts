84387fda3ee2e3a09e51f12f20ddfc537c976e4a
nithitsuki <nithilanr@gmail.com>
2025-06-25 23:57:23 +0530

    version bump

--- wxt-port/wxt.config.ts
+++ wxt-port/wxt.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   manifest: {
     name: "Amrita Attendance Fetcher",
     description: "Capture attendance data from Amrita University's student portal.",
-    version: "2.0.0",
+    version: "3.0.0",
     permissions: [
       "tabs",
       "cookies", 
